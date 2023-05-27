9c5c5e526a526c2742513428c047c8bdc3f47977
Marcus Torres <marcuslorenlopes@gmail.com>
2023-05-27 08:54:30 +0000

    Pending changes exported from your codespace

--- app/root.jsx
+++ app/root.jsx
@@ -1,11 +1,31 @@
-import { Outlet, LiveReload, Link } from 'remix';
-import globalStylesUrl from '/styles/global.css'
+import { Outlet, LiveReload, Link, Links, Meta, useLoaderData } from '@remix-run/react'
+import globalStylesUrl from './styles/global.css';
+import { getUser } from './utils/session.server';
+
+export const links = () => [{ rel: 'stylesheet', href: globalStylesUrl }];
+
+export const meta = () => {
+  const description = 'A cool Blog built with remix';
+  const keywords = 'remix, react, javascript';
+  return {
+    description,
+    keywords
+  }
+}
+
+export const loader = async ({ request }) => {
+  const user = await getUser(request)
+  const data = {
+    user
+  }
+  return data
+}
 
 export default function App() {
   return (
     <Document>
       <Layout>
-      <Outlet />
+        <Outlet />
       </Layout>
     </Document>
   );
@@ -15,41 +35,75 @@ export function Document({ children, title }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href={globalStylesUrl}/>
-
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
         <title>{title ? title : 'Remix App'}</title>
+        <Links />
       </head>
-
       <body>
         {children}
-        <LiveReload />
+
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
   );
 }
 
 
-function Layout({children}){
+function Layout({ children }) {
+
+  const { user } = useLoaderData();
+
   return (
     <>
-<nav className="navbar">
+      <nav className="navbar">
 
-  <Link to="/" className="logo">
-Remix
-  </Link>
+        <Link to="/" className="logo">
+          Remix
+        </Link>
 
-  <ul className="nav">
+        <ul className="nav">
 
-    <li>
-      <Link to="/posts">Posts</Link>
-    </li>
-  </ul>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
 
-  <div className="container">
-    {children}
-  </div>
-   
-</nav>
+          {user ? (
+            <li>
+              <form action="/logout"
+                method="POST"
+              >
+                <button className="btn" type="submit">
+                  Logout {user.username}
+                </button>
+
+              </form>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+
+
+        </ul>
+
+      </nav>
+      <div className="container">s
+        {children}
+      </div>
     </>
+  )
+}
+
+export function ErrorBoundary({ error }) {
+  console.log(error);
+  return (
+    <Document>
+      <Layout></Layout>
+      <h1>Error</h1>
+      <p>{error.message}</p>
+    </Document>
   )
 }
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
 
-function index(){
+function index() {
   return (
     <div>
+      <h1>Welcome</h1>
 
-<h1><Welcome/h1>
+      <p>Remix is a full stack web framework by the creators of React Router.
+        This is a simple blog app from the Traversy Media Remix crach course.
+      </p>
     </div>
   )
 }
